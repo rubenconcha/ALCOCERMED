a771d9851966b72c00896c9b1e97d11e91e1fe6d
rubenconcha <pichon4488@gmail.com>
2026-05-26 16:29:09 -0400

    f

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'alcocermed-main-v19';
+const CACHE_NAME = 'alcocermed-main-v20';
 const APP_SHELL = [
     './',
     './index.html',
-    './styles.css?v=19',
+    './styles.css?v=20',
     './script.js?v=4',
     './config.js',
     './device_guard.js',
